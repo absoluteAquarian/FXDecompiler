bfdd678968729719a4da0d937094b11503e14986
absoluteAquarian <jruss88@me.com>
2022-04-11 19:55:03 -0500

    Add project files.

---
+++ src/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FXDecompiler {
+	public static class Program {
+		public static void Main(string[] args) {
+			if(args.Length == 0) {
+				Console.WriteLine("Usage: FXDecompiler.exe <file>");
+				return;
+			}
+
+			
+		}
+	}
+}
